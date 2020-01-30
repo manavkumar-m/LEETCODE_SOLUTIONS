568c5788d75936e26e1a12ca67d4049259da1da1
MANAV KUMAR M <55437126+manavkumar-m@users.noreply.github.com>
2020-01-30 14:27:39 +0530

    Update 287.cpp

--- 287.cpp
+++ 287.cpp
@@ -2,12 +2,10 @@ Given an array nums containing n + 1 integers where each integer is between 1 an
 Assume that there is only one duplicate number, find the duplicate one.
 
 Example 1:
-
 Input: [1,3,4,2,2]
 Output: 2
 
 Example 2:
-
 Input: [3,1,3,4,2]
 Output: 3
 
@@ -18,8 +16,6 @@ Note1:
 4. There is only one duplicate number in the array, but it could be repeated more than once.
 
 _______________________________________________________________________________________________________
-
--->>SOLUTION<<--
 
 class Solution
 {
